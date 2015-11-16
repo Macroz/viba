44905233021998c72639b68f8ffdb2d73beb5260
Markku Rontu <markku.rontu@iki.fi>
2015-11-16 22:23:33 +0200

    Cleanup

--- src/viba/core.clj
+++ src/viba/core.clj
@@ -3,20 +3,3 @@
   (:require [viba.ops :as ops :refer :all])
   (:require [clojure.core.async :as async :refer [<! go-loop]]))
 
-(defn analyze-chunk [x]
-  (println "analyzing" (:i x))
-  x)
-
-(defn play-chunk [x]
-  (println "playing" (:i x))
-  x)
-
-(defn test-pipe [filename]
-  (let [reader (audio/read-file filename)
-        analyzer (async/map analyze-chunk [reader] 16)
-        player (async/map play-chunk [analyzer])]
-    (go-loop []
-      (when-let [x (<! player)]
-        (println "done-with" (:i x))
-        (recur)))
-    player))
